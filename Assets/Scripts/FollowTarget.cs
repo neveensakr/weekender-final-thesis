6429f189cf92e9d990f81430a6cd259b267bdabd
neveensakr <64664334+neveensakr@users.noreply.github.com>
2024-12-07 11:08:08 +0100

    Add MainCharacter Cinematic Intro
    This adds the main character cinematic intro, where the character gets off the yacht and into the lounge. To do this, I added the Cinemachine package for the virtual cameras with dolly tracks.

--- Assets/Scripts/FollowTarget.cs
+++ Assets/Scripts/FollowTarget.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class FollowTarget : MonoBehaviour
 {
     [SerializeField] private Transform _target;
